66841a062eaaa2ebf6f7d24f5b184c432cf781e3
kirillkisly <kislykirill3@gmail.com>
2018-06-13 12:45:59 +0300

    Changing the primary key to an int value

---
+++ ibby-cms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ibby_cms.Startup))]
+namespace ibby_cms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
