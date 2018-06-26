b3e2b298c659600672d6c53dca83491711c0288a
kirillkisly <kislykirill3@gmail.com>
2018-06-26 15:51:51 +0300

    Creating view layer

---
+++ ibby-cms.Web/Models/PageContentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ibby_cms.Models
+{
+    public class PageContentViewModel
+    {
+        public int? SeoID { get; set; }
+        public string HtmlContent { get; set; }
+        public string Content { get; set; }
+        public string Url { get; set; }
+        public string Header { get; set; }
+    }
+}
---
+++ ibby-cms.Web/PageContentModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ibby_cms.Common.Manager.Interfaces;
+using ibby_cms.Common.Manager.Services;
+using Ninject.Modules;
+
+namespace ibby_cms
+{
+    public class PageContentModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IPageContentService>().To<PageContentService>();
+        }
+    }
+}
